64e4d240d89dda2d650c04b73044735721567b27
Adam Johnson <adamjohnson@apple.com>
2020-06-16 12:14:31 -0700

    1.2

--- tooltipper.js
+++ tooltipper.js
@@ -1,6 +1,6 @@
 /*
 
-Tooltipper v1.1
+Tooltipper v1.2
 Simple tooltips on hover. Compatible with modern browsers only (ES6+).
 https://github.com/adamwjohnson5/tooltipper
 By Adam Johnson
@@ -26,43 +26,41 @@ class Tooltipper {
     }
     
     init() {
-        if (! navigator.userAgent.match(/iPhone|iPad|iPod/i) && ! navigator.userAgent.match(/Android/i)) { // DT only
-            // Style container and add to DOM
-            this.tooltipper.style.position = 'fixed';
-            this.tooltipper.style.zIndex = 999;
-            this.tooltipper.style.display = 'none';
-            this.tooltipper.style.margin = '8px';
-            this.tooltipper.style.padding = '4px';
-            this.tooltipper.style.paddingLeft = '6px';
-            this.tooltipper.style.paddingRight = '6px';
-            this.tooltipper.style.fontSize = '12px';
-            this.tooltipper.style.fontFamily = 'sans-serif';
-            this.tooltipper.style.backgroundColor = this.bgColor;
-            this.tooltipper.style.color = this.txtColor;
-            this.tooltipper.style.borderRadius = this.bdrRadius;
-            document.querySelector('body').after(this.tooltipper);
-            
-            // Mouse move
-            window.addEventListener('mousemove', (e) => {
-                const windowWidth = window.innerWidth;
-                let i = (windowWidth / 2);
-                if (e.pageX > i) { // Right side of window
-                    this.tooltipper.style.left = 'auto';
-                    this.tooltipper.style.right = (windowWidth - e.pageX) + 'px';
-                    this.tooltipper.style.boxShadow = '-1px 1px 3px rgba(0, 0, 0, 0.25)';
-                } else { // Left side of window
-                    this.tooltipper.style.right = 'auto';
-                    this.tooltipper.style.left = e.pageX + 'px';
-                    this.tooltipper.style.boxShadow = '1px 1px 3px rgba(0, 0, 0, 0.25)';
-                }
-                this.tooltipper.style.top = e.pageY + 'px';
-            });
-            
-            // Loop all items in DOM with a tooltipper attr
-            const tooltips = document.querySelectorAll('a[data-tooltipper]');
-            for (let x = 0; x < tooltips.length; x++) {
-                this.add(tooltips[x]);
+        // Style container and add to DOM
+        this.tooltipper.style.position = 'fixed';
+        this.tooltipper.style.zIndex = 999;
+        this.tooltipper.style.display = 'none';
+        this.tooltipper.style.margin = '8px';
+        this.tooltipper.style.padding = '4px';
+        this.tooltipper.style.paddingLeft = '6px';
+        this.tooltipper.style.paddingRight = '6px';
+        this.tooltipper.style.fontSize = '12px';
+        this.tooltipper.style.fontFamily = 'sans-serif';
+        this.tooltipper.style.backgroundColor = this.bgColor;
+        this.tooltipper.style.color = this.txtColor;
+        this.tooltipper.style.borderRadius = this.bdrRadius;
+        document.querySelector('body').after(this.tooltipper);
+
+        // Mouse move
+        window.addEventListener('mousemove', (e) => {
+            const windowWidth = window.innerWidth;
+            let i = (windowWidth / 2);
+            if (e.pageX > i) { // Right side of window
+                this.tooltipper.style.left = 'auto';
+                this.tooltipper.style.right = (windowWidth - e.pageX) + 'px';
+                this.tooltipper.style.boxShadow = '-1px 1px 3px rgba(0, 0, 0, 0.25)';
+            } else { // Left side of window
+                this.tooltipper.style.right = 'auto';
+                this.tooltipper.style.left = e.pageX + 'px';
+                this.tooltipper.style.boxShadow = '1px 1px 3px rgba(0, 0, 0, 0.25)';
             }
+            this.tooltipper.style.top = e.pageY + 'px';
+        });
+
+        // Loop all items in DOM with a tooltipper attr
+        const tooltips = document.querySelectorAll('a[data-tooltipper]');
+        for (let x = 0; x < tooltips.length; x++) {
+            this.add(tooltips[x]);
         }
     }
     
